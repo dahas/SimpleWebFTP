55cb46c48867fd9d7399eb265e91dfa8e0755513
dahas <dahas@gmx.de>
2016-04-10 22:59:13 +0200

    .

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 process.on('uncaughtException', function (err) {
     console.log(err);
 });
 
 var express = require("./config/express");
-module.exports = express().listen(9001);
-
-console.log('Server running on localhost:9001 in mode: ' + process.env.NODE_ENV)
+module.exports = express().listen(server_port, server_ip_address, function () {
+    console.log("Listening on " + server_ip_address + ", server_port " + server_port + ", in mode '" + process.env.NODE_ENV + "'.")
+});
